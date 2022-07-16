b55f032016d27c7cba1e1a9350fb2b4f67b660fb
fengchuxiao <chuxiaof@uchicago.edu>
2022-07-16 00:39:16 -0500

    validate username in edit profile form

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
-from app import routes, models, errors
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from config import Config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import RotatingFileHandler
-import os
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -28,3 +30,5 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+from app import routes, models, errors
