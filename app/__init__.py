72a645d5860a5692a0ffd8843aea22a2c908df47
fengchuxiao <chuxiaof@uchicago.edu>
2022-07-17 16:17:42 -0500

    translations

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from config import Config
-from flask import Flask
+from flask import Flask, request
+from flask_babel import Babel
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
@@ -12,15 +13,18 @@ from flask_login import LoginManager
 from logging.handlers import RotatingFileHandler
 
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -51,6 +55,8 @@ if not app.debug:
     app.logger.info('Microblog startup')
 
 
-
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 from app import routes, models, errors
